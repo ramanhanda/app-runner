36cafabc7dedd22329e908f2926178544a8b0ea5
Raman Handa <ramanhanda2050@gmail.com>
2025-01-31 17:29:11 +0530

    added docker file and api changes

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
-
+import time
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def root_fn():
+    return 'API is working fine'
+
+@app.route('/time')
+def time_now():
+    t = time.localtime()
+    return t
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
